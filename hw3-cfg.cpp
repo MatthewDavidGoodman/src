38555fccc1468ea2362916d60d7cd41638cb4301
MatthewDavidGoodman <67522125+MatthewDavidGoodman@users.noreply.github.com>
2021-11-24 22:42:07 -0500

    Update hw3-cfg.cpp

--- hw3-cfg.cpp
+++ hw3-cfg.cpp
@@ -8,7 +8,6 @@
 #include "hw3-utils.h"
 #include "llvm/IR/CFG.h"
 #include "llvm/IR/Dominators.h"
-
 using namespace llvm;
 
 #define DEBUG_TYPE "hw3_cfg"
@@ -28,44 +27,43 @@ struct hw3_cfg : public FunctionPass {
     //ofile.printKeyBlock(BB);
     //prints name of function
     errs() << "Visiting function " << F.getName();
-    //iterate over basic blocks 
+    //iterate over basic blocks
     BasicBlock &BBF = F.back();
-    
-    for (BasicBlock &BB : F) {
+    DominatorTree DT = DominatorTree(F);
+    for (BasicBlock &BB : F){
         //Checking to see if BB branches, do we have to handle BB branchign into 1 and into 2 differently?
-        
-        //is there a way to write the below code using successors
-        
         //get terminator instruction of basic block and iterate through its succesors
-        
-        
-        for (BasicBlock *Pred : predecessors(BB)) {
-
-        //handle 1 predecesor 
-
-        //handle 2 predecessors   
-
-        BasicBlock& BB1 = BB;
-        BasicBlock& BB2 = BB.getSuccessor();
-        ofile.printEdge(BB1, BB2, OFile::EdgeType::T);
+        for (j)
+        Instruction *I = BB.getTerminator();
+        //handle 2 successors
+        if(I->getNumSuccessors() == 2)
+        {
+        BasicBlock BB1 =(I->getSuccessor(0));
+        BasicBlock BB2 =(I->getSuccessor(1));
+        ofile.printEdge(BB, *BB1, OFile::EdgeType::T);
+        ofile.printEdge(BB, *BB2, OFile::EdgeType::T);
         }
-        
+        //handle 1 successors
+        else if (I->getNumSuccessors() == 1)
+        {
+        BasicBlock BB1 = I->getSuccessor(0);
+        ofile.printEdge(BB, *BB1, OFile::EdgeType::T);
+        } 
         for (Instruction &II : BB) {
         // Iterate over each Instructions in Basic Blocks
-        // Dynamically cast Instruction to CallInst. 
+        // Dynamically cast Instruction to CallInst.
         // This step will return false for any instruction which is not a CallInst
             if(CallInst *CI = dyn_cast<CallInst>(&II)) {
                 // Print out that we have encountered a call instruction
                 errs() << "Encountered a call instruction " << CI << "\n";
                 //Print out function name
                 outs() << " |-" << CI->getCalledFunction()->getName() << "\n";
-                if(dominates(CI, BBF)){
-                    errs() << "Encountered a key block " << CI << "\n";   
+                if(DT.dominates(&BB, &II)){
+                    errs() << "Encountered a key block " << CI << "\n";
                     ofile.printKeyBlock(BB);
                 }
              }
-        }
-      }
+        }}
     ofile.printKeyBlock(BBF);
     return false;
   }
